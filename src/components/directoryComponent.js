fc3c1e7b1767668d9530341ba301d23027ea68d4
MiaMoustafa <gd.lamiaa@gmail.com>
2021-10-15 11:19:29 -0400

    pushing after fixing upstream error

--- src/components/directoryComponent.js
+++ src/components/directoryComponent.js
@@ -1,39 +1,28 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDirectoryItem({ campsite }) {
   return (
     <Card>
-      <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-      <CardImgOverlay>
-        <CardTitle>{campsite.name}</CardTitle>
-      </CardImgOverlay>
+      <Link to={`/directory/${campsite.id}`}>
+        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <CardImgOverlay>
+          <CardTitle>{campsite.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
     </Card>
   );
 }
 
 function Directory(props) {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //   };
-  // }
-
-  //   renderSelectedCampsite(campsite) {
-  //     if (campsite) {
-  //       return (
-  //         <Card>
-  //           <CardImg top src={campsite.image} alt={campsite.name} />
-  //           <CardBody>
-  //             <CardTitle>{campsite.name}</CardTitle>
-  //             <CardText>{campsite.description}</CardText>
-  //           </CardBody>
-  //         </Card>
-  //       );
-  //     }
-  //     return <div />;
-  //   }
-
   const directory = props.campsites.map((campsite) => {
     return (
       <div key={campsite.id} className="col-md-5 m-1">
@@ -44,6 +33,18 @@ function Directory(props) {
 
   return (
     <div className="container">
+      <div className="row">
+        <div className="col">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Directory</BreadcrumbItem>
+          </Breadcrumb>
+          <h2>Directory</h2>
+          <hr />
+        </div>
+      </div>
       <div className="row">{directory}</div>
     </div>
   );
